8e029d5e49818e51a6ca3fbf027e9512851e2ddb
venilson2 <venilson2009@hotmail.com>
2023-11-28 19:51:48 -0300

    feat: ajustado script para salvar no db

--- node/index.js
+++ node/index.js
@@ -2,9 +2,48 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-// Define a route for the root path
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
+const config = {
+  host: 'db',
+  user: 'root',
+  password: 'root',
+  database: 'nodedb'
+}
+
+const mysql = require('mysql');
+
+const executeQuery = (query, params) => {
+  return new Promise((resolve, reject) => {
+    const connection = mysql.createConnection(config);
+    connection.query(query, params, (err, results) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve({'docker': '<h1>Full Cycle Rocks!</h1>', 'results': results});
+      }
+      connection.end();
+    })
+  });
+}
+
+app.get('/', async (req, res) => {
+  try {
+    const person = {
+      name: 'Venilson'
+    };
+
+    const insertQuery = 'INSERT INTO tb_peoples (name) VALUES (?)';
+
+    const selectQuery = 'SELECT * FROM tb_peoples';
+
+    await executeQuery(insertQuery, [person.name]);
+
+    const result = await executeQuery(selectQuery);
+
+    res.json(result);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Erro interno do servidor');
+  }
 });
 
 // Start the server
